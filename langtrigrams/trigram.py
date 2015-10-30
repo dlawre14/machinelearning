313f4becdd70eb6bde0f9eef531d5114bfe3bced
dlawre14 <dlawre14@turing.slu.edu>
2015-10-30 17:25:24 -0500

    more work on trigrams, old contains some data

--- langtrigrams/trigram.py
+++ langtrigrams/trigram.py
@@ -4,16 +4,25 @@ import sys
 import os
 import itertools
 
-def readInTrigrams(filename):
+import time
+import pickle
+
+def readInTrigrams(filename, probconv=True):
     '''
     Reads a language trigram file and returns a dictionary of trigram:count
     '''
 
     tricounts = {}
+    total = 0
     with open(filename, 'r') as f:
         for line in f:
             line = line.split()
             tricounts[line[1]] = int(line[0])
+            total += int(line[0])
+
+    if probconv:
+        for key in tricounts:
+            tricounts[key] = tricounts[key]/total
 
     return tricounts
 
@@ -34,9 +43,9 @@ def distLang(l1, l2):
 
     return dist
 
-def mergeFiles(f1name, f2name):
-    f1tri = readInTrigrams('trigrams/'+f1name)
-    f2tri = readInTrigrams('trigrams/'+f2name)
+def mergeFiles(f1name, f2name, outname):
+    f1tri = readInTrigrams('trigrams/'+f1name, False)
+    f2tri = readInTrigrams('trigrams/'+f2name, False)
 
     keys = set()
 
@@ -50,7 +59,7 @@ def mergeFiles(f1name, f2name):
     for key in keys:
         mergetri[key] = f1tri.get(key,0) + f2tri.get(key,0)
 
-    with open('trigrams/'+f1name.rstrip('.txt')+'_'+f2name.rstrip('.txt')+'.txt', 'w') as f:
+    with open('trigrams/'+outname, 'w') as f:
         for key in mergetri:
             f.write(str(mergetri[key]) + ' ' + key + '\n')
 
@@ -58,4 +67,37 @@ def mergeFiles(f1name, f2name):
 
 
 if __name__ == '__main__':
-    #TODO: Write code to find min distance, merge, repeat
+    #Generate root dist matrix
+    start = time.time()
+    langs = os.listdir('trigrams')
+
+    parentalid = {}
+
+    while len(langs) > 1:
+        minpair = None
+        mindist = 1000000000000000
+        for pairs in itertools.combinations(langs, 2):
+            l1 = readInTrigrams('trigrams/'+pairs[0])
+            l2 = readInTrigrams('trigrams/'+pairs[1])
+            dist = distLang(l1,l2)
+            if dist < mindist:
+                mindist = dist
+                minpair = (pairs[0], pairs[1])
+
+        print ('Pairing ' + str(minpair) + ' langs left ' + str(len(langs)-1))
+
+        mergeFiles(minpair[0], minpair[1], str(100 - len(langs))+'.txt')
+
+        with open('mergehist.txt', 'a') as f:
+            f.write('Merge #' + str(len(langs) - 100) + ' ' + minpair[0] + ' ' + minpair[1] + '\n')
+        with open('nameref.txt', 'a') as f:
+            f.write('Pairing ' + str(minpair) + ' --> ' + str(100 - len(langs))+'.txt\n')
+
+        parentalid[minpair[0]] = 100 - len(langs)
+        parentalid[minpair[1]] = 100 - len(langs)
+
+        langs.append(str(100 - len(langs))+'.txt')
+        langs.remove(minpair[0])
+        langs.remove(minpair[1])
+
+    pickle.dump(parentalid, open('tree.p', 'wb'))
